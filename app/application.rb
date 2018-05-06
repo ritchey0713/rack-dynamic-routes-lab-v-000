8e8489128853bda340688e69296ca27c8c12615d
David Ritchey <ritchey0713@gmail.com>
2018-05-06 03:56:08 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -4,6 +4,11 @@ class Application
     resp = Rack::Response.new 
     req = Rack::Request.new(env)
     
+      if req.path.match(/items/)
+      @@items.each do |item|
+        resp.write "#{item}\n"
+      end
+    
   end 
   
   
