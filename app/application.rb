b53001916b9751a00d402879b1ec8ae2e0daf99b
David Ritchey <ritchey0713@gmail.com>
2018-05-06 04:09:40 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -11,7 +11,7 @@ class Application
       if item 
         resp.write "#{item.price}"
       else 
-        resp.write "item not found"
+        resp.write "Item not found"
         resp.status = 400 
       end 
       
